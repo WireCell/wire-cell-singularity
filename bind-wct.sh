a39e90ce48495bc252420cd645517a3e72721431
Brett Viren <brett.viren@gmail.com>
2018-08-23 13:57:50 -0400

    Flesh out wclsdev.rc to provide Bash functions to automate UPS shenigans.

--- bind-wct.sh
+++ bind-wct.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 simg="$1" ; shift
 native_dir="$1" ; shift
+rcfile="$1"; shift
+
+if [ -n "$rcfile" ] ; then
+    rcfile="--rcfile $rcfile"
+fi
+if [ ! -d "$native_dir" ] ; then
+    echo "bind-wct: directory to bind does not exist, making it now"
+    mkdir -p "$native_dir"
+fi
+
 bind_target=$(singularity exec $simg \
     bash -c 'source /usr/local/ups/setup; setup wirecell dev -q e15:prof; echo $WIRECELL_FQ_DIR')
-singularity shell --bind "$native_dir":"$bind_target" $simg 
+singularity exec --bind "$native_dir":"$bind_target" $simg /bin/bash $rcfile
+
 
 
